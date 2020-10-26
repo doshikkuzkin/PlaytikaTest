b901e6e00f6a19c7f09265bae407aece731402a6
dariak <dariak@playtika.com>
2020-10-26 16:58:47 +0300

    Best Practices

---
+++ Skyroads/Assets/Editor/AudioPostprocessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+public class AudioPostprocessor : AssetPostprocessor
+{
+    private float minSize = 200;
+    private float maxSize = 5120;
+    
+    private void OnPreprocessAudio()
+    {
+        var fileInfo = new FileInfo(assetPath);
+        float fileSize = (float)fileInfo.Length/1024;
+        
+        AudioImporter audioImporter = (AudioImporter)assetImporter;
+
+        audioImporter.loadInBackground = true;
+        audioImporter.preloadAudioData = true;
+        
+        AudioImporterSampleSettings sampleSettings = audioImporter.defaultSampleSettings;
+        
+        if (fileSize <= minSize)
+        {
+            sampleSettings.loadType = AudioClipLoadType.DecompressOnLoad;
+        }
+        else if (fileSize > minSize && fileSize < maxSize)
+        {
+            sampleSettings.loadType = AudioClipLoadType.CompressedInMemory;
+        }
+        else
+        {
+            sampleSettings.loadType = AudioClipLoadType.Streaming;
+        }
+
+        audioImporter.defaultSampleSettings = sampleSettings;
+    }
+}
